7e8acdb94f739486a6c9ee34f097dfe7dbc7595b
Allan Wanderley <awallan259@gmail.com>
2020-08-11 05:39:55 -0300

    Test: Changed tests for accept boolean results - partial

--- test/exists.test.js
+++ test/exists.test.js
@@ -3,62 +3,61 @@ const jsvalidators = require('../index')
 
 describe('Exists elements', () => {
     it('in Array', () => {
-        try {
-            const _validNumbersArray = [10, 12, 15]
-            const _validObjectsArray = [{age: 10}, {age: 12}, {age: 15}]
-            const _validStringsArray = ['spencer', 'oliver', 'agatha', 'allan']
-            const _validBooleanArray = [true, false, true, true]
-            const _validArrayArray = [[12,23], ['barbara', 'allan'], [[12,23], {key: 'key'}]]
-            
-            jsvalidators.exists(_validNumbersArray, "Not exists value in array numbers")
-            jsvalidators.exists(_validObjectsArray, "Not exists value in array objects")
-            jsvalidators.exists(_validStringsArray, "Not exists value in array strings")
-            jsvalidators.exists(_validBooleanArray, "Not exists value in array boolean")
-            jsvalidators.exists(_validArrayArray, "Not exists value in array of arrays")
-
-            assert.ok('ok')
-        } catch (errMessage) {
-            assert.fail(errMessage)
+        const _validNumbersArray = [10, 12, 15]
+        const _validObjectsArray = [{age: 10}, {age: 12}, {age: 15}]
+        const _validStringsArray = ['spencer', 'oliver', 'agatha', 'allan']
+        const _validBooleanArray = [true, false, true, true]
+        const _validArrayArray = [[12,23], ['barbara', 'allan'], [[12,23], {key: 'key'}]]
+        
+        if (!jsvalidators.exists(_validNumbersArray)) {
+            assert.fail("Not exists value in array numbers")
         }
+
+        if (!jsvalidators.exists(_validObjectsArray)) {
+            assert.fail("Not exists value in array objects")
+        }
+
+        if (!jsvalidators.exists(_validStringsArray)) {
+            assert.fail("Not exists value in array strings")
+        }
+
+        if (!jsvalidators.exists(_validBooleanArray)) {
+            assert.fail("Not exists value in array boolean")
+        }
+
+        if (!jsvalidators.exists(_validArrayArray)) {
+            assert.fail("Not exists value in array of arrays")
+        }
+
+        assert.ok('ok')
     });
 
     it('in String', () => {
-        try {
-            const _value = 'Sample string'
-            jsvalidators.exists(_value, 'Not exists value in string test')
-            assert.ok('ok')
-        } catch (errMessage) {
-            assert.fail(errMessage)
+        const _value = 'Sample string'
+        if (!jsvalidators.exists(_value)) {
+            assert.fail('Not exists value in string test')
         }
+        
+        assert.ok('ok')
     })
 });
 
 describe('Not exists elements', () => {
     it('in Array', () => {
-        try {
-            const _invalidArray = []
-            jsvalidators.exists(_invalidArray, "The array is empty")
-            assert.fail('not captured error')
-        } catch (errMessage) {
-            if(errMessage === 'The array is empty'){
-                assert.ok(`ok: ${errMessage}`)
-            }else {
-                assert.fail(errMessage)
-            }
+        const _invalidArray = []
+        if (!jsvalidators.exists(_invalidArray)) {
+            assert.ok("The array is empty")
+        } else {
+            assert.fail('The array is not empty')
         }
     })
 
     it('in String', () => {
-        try {
-            const _value = ''
-            jsvalidators.exists(_value, 'The string value is not defined')
-            assert.fail('not captured error')
-        } catch (errMessage) {
-            if(errMessage === 'The string value is not defined') {
-                assert.ok(`ok: ${errMessage}`)
-            }else{
-                assert.fail(errMessage)
-            }
+        const _value = ''
+        if (!jsvalidators.exists(_value)) {
+            assert.ok('The string value is not defined')
+        } else {
+            assert.fail('The string value is defined')
         }
     })
 })
